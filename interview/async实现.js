498284bb24c6d1988b03062b0788b482ce7f4694
tsunemori-akane <39367561+tsunemori-akane@users.noreply.github.com>
2022-08-03 17:45:26 +0800

    -

---
+++ interview/async实现.js
@@ -0,0 +1,34 @@
+const {readFile} = require('fs/promises')
+const path = require('path')
+const classFile = path.join(__dirname,"name.txt")
+
+function  * read() {
+    // let filepath = yield readFile(classFile, 'utf8')
+    return yield readFile(classFile, 'utf8')
+}
+
+function co(it) {
+    return new Promise((resolve, reject) => {
+        let next = function(data) {
+            console.log('data:', data)
+            let {value, done} = it.next(data)
+            if(done) {
+                console.log('value_done:', value)
+                resolve(value)
+            } else {
+                console.log('value:', value)
+                Promise.resolve(value).then(res => {
+                    console.log('res:', res)
+                    next(res)
+                })
+            }
+        }
+        next()
+        
+    })
+}
+
+let res = co(read())
+res.then(res => {
+    console.log('end:', res)
+})
